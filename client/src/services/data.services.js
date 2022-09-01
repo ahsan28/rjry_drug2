ec51d397e5388e44aff3ce2d4bcf5d9a6a9a1f7b
ahsan28 <ahabib.j@gmail.com>
2022-09-01 11:18:14 +0600

    add auth headers

--- client/src/services/data.services.js
+++ client/src/services/data.services.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
+import authHeader from './auth.header';
 
 const api = 'http://localhost:5000/';
 
 const getAll = () => {
-    return axios.get(api+'getAll');
+    return axios.get(api+'getAll', { headers: authHeader() });
 }
 
 const read = (title) => {
-    return axios.get(api+title);
+    return axios.get(api+title, { headers: authHeader() });
 }
 
 const createAll = () => {
-    return axios.get(`${api}createAll`);
+    return axios.get(`${api}createAll`, { headers: authHeader() });
 }
 
 const functions = {
--- client/src/services/media.services.js
+++ client/src/services/media.services.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import authHeader from './auth.header';
 
 const api = 'http://localhost:5000/media';
 
 const getAll = () => {
     console.log('MediaService.js')
-    return axios.get(api+'/getAll');
+    return axios.get(api+'/getAll', { headers: authHeader() });
 }
 
 const functions = {
