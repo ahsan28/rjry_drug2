bef0dbd7b316e17ff00bb888b47a80071b811abc
ahsan28 <ahabib.j@gmail.com>
2023-05-20 10:48:38 +0600

    type

--- server/models/activity.model.js
+++ server/models/activity.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const ActivitySchema = new mongoose.Schema({
+    type: { type: String },
     name: { type: String },
     title: { type: String },
     description: { type: String },
