d0772e422abc16194aff726500b4abe72f7fb341
ahabib28 <ahabib.j@gmail.com>
2024-02-28 17:50:19 +0800

    Update SettingsForm.js

--- client/src/components/Forms/SettingsForm.js
+++ client/src/components/Forms/SettingsForm.js
@@ -1,40 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import DataService from "../../services/data.services";
 import MediaService from "../../services/media.services";
-import {
-  FormControl,
-  InputLabel,
-  Input,
-  FormHelperText,
-  Button,
-  PaginationItem,
-  Box,
-  TextField,
-  Select,
-  Typography,
-  MenuItem,
-  Paper,
-  Card,
-  CardMedia,
-  CardActions,
-  Container,
-  FormLabel,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-} from "@mui/material";
+import {FormControl,InputLabel,Input,FormHelperText,Button,PaginationItem,Box,TextField,Select,Typography,MenuItem,Paper,Card,CardMedia,CardActions,Container,FormLabel,RadioGroup,FormControlLabel,Radio,} from "@mui/material";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import FileUpload from "react-mui-fileuploader";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  ListItemSecondaryAction,
-  IconButton,
-  ListSubheader,
-  Divider,
-} from "@mui/material";
+import {  List,  ListItem,  ListItemText,  ListItemIcon,  ListItemSecondaryAction,  IconButton,  ListSubheader,  Divider,} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import UserService from "../../services/user.services";
 import LinearProgress from "@mui/material";
@@ -42,29 +12,7 @@ import { UserContext } from "../../UserContext";
 import ViewImage from "../Hooks/ViewImage";
 import { SketchPicker } from "react-color";
 
-const fonts = [
-  "serif",
-  "sans-serif",
-  "monospace",
-  "cursive",
-  "system-ui",
-  "roboto",
-  "arial",
-  "helvetica",
-  "georgia",
-  "courier",
-  "impact",
-  "garamond",
-  "bookman",
-  "arial black",
-  "avant garde",
-  "century gothic",
-  "optima",
-  "rockwell",
-  "tahoma",
-  "times new roman",
-  "verdana",
-];
+const fonts = ["serif","sans-serif","monospace","cursive","system-ui","roboto","arial","helvetica","georgia","courier","impact","garamond","bookman","arial black","avant garde","century gothic","optima","rockwell","tahoma","times new roman","verdana"];
 // 7 to 22 step 1
 const fontSizes = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 const themes = [
@@ -259,198 +207,6 @@ const themes = [
   },
 ];
 
-const thm_old = [
-  {
-    name: "Default",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#347a2a",
-      info: "#0275d8",
-      warning: "#e67e22",
-      error: "#d32f2f",
-
-      primary: "#005cb2", // Deeper blue
-      secondary: "#ffb300", // Contrasting orange
-      primaryText: "#FFFFFF",
-      secondaryText: "#FAFAFA",
-
-      body: "#FFFFFF",
-      paper: "#F5F5F5",
-      text: "#000000",
-      link: "#1E88E5",
-
-      header: "#E0E8F9", // Lighter shade of primary
-      footer: "#C4D3EC", // Slightly different lighter shade
-      stripe: "#7A96C9", // More muted shade related to primary
-      headerText: "#212121",
-      footerText: "#212121",
-      stripeText: "#FFFFFF",
-    },
-  },
-  {
-    name: "Dark",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#4CAF50",
-      info: "#2196F3",
-      warning: "#FFC107",
-      error: "#F44336",
-
-      primary: "#90CAF9",
-      secondary: "#CE93D8",
-      primaryText: "#000000",
-      secondaryText: "#000000",
-
-      body: "#FFFFFF",
-      paper: "#424242",
-      text: "#000000",
-      link: "#BBDEFB",
-
-      header: "#333333",
-      footer: "#333333",
-      stripe: "#6A1B9A",
-      headerText: "#E0E0E0",
-      footerText: "#E0E0E0",
-      stripeText: "#FFFFFF",
-    },
-  },
-  {
-    name: "Classic Blue",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#81C784",
-      info: "#64B5F6",
-      warning: "#FFD54F",
-      error: "#E57373",
-
-      primary: "#0D47A1",
-      secondary: "#FF8A65",
-      primaryText: "#FFFFFF",
-      secondaryText: "#000000",
-
-      body: "#FFFFFF",
-      paper: "#BBDEFB",
-      text: "#000000",
-      link: "#FFC107",
-
-      header: "#0D47A1",
-      footer: "#0D47A1",
-      stripe: "#5C6BC0",
-      headerText: "#FFFFFF",
-      footerText: "#FFFFFF",
-      stripeText: "#FFFFFF",
-    },
-  },
-  {
-    name: "Baby Blue",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#A5D6A7",
-      info: "#81D4FA",
-      warning: "#FFE082",
-      error: "#EF9A9A",
-
-      primary: "#4FC3F7",
-      secondary: "#F48FB1",
-      primaryText: "#000000",
-      secondaryText: "#FFFFFF",
-
-      body: "#FFFFFF",
-      paper: "#B3E5FC",
-      text: "#000000",
-      link: "#FF7043",
-
-      header: "#29B6F6",
-      footer: "#29B6F6",
-      stripe: "#4DD0E1",
-      headerText: "#FFFFFF",
-      footerText: "#FFFFFF",
-      stripeText: "#FFFFFF",
-    },
-  },
-  {
-    name: "Green",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#81C784",
-      info: "#4DD0E1",
-      warning: "#FFD54F",
-      error: "#E57373",
-
-      primary: "#66BB6A",
-      secondary: "#26A69A",
-      primaryText: "#FFFFFF",
-      secondaryText: "#000000",
-
-      body: "#FFFFFF",
-      paper: "#C8E6C9",
-      text: "#000000",
-      link: "#FFEB3B",
-
-      header: "#A5D6A7",
-      footer: "#A5D6A7",
-      stripe: "#43A047",
-      headerText: "#004D40",
-      footerText: "#004D40",
-      stripeText: "#FFFFFF",
-    },
-  },
-  {
-    name: "Orange",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#AED581",
-      info: "#4FC3F7",
-      warning: "#FFB74D",
-      error: "#FF8A65",
-
-      primary: "#FFA726",
-      secondary: "#FF7043",
-      primaryText: "#000000",
-      secondaryText: "#FFFFFF",
-
-      body: "#FFFFFF",
-      paper: "#FFE0B2",
-      text: "#000000",
-      link: "#4CAF50",
-
-      header: "#FFCCBC",
-      footer: "#FFCCBC",
-      stripe: "#FB8C00",
-      headerText: "#BF360C",
-      footerText: "#BF360C",
-      stripeText: "#FFFFFF",
-    },
-  },
-  {
-    name: "Purple",
-    fontFamily: "sans-serif",
-    sheds: {
-      success: "#81C784",
-      info: "#64B5F6",
-      warning: "#FFD54F",
-      error: "#E57373",
-
-      primary: "#9575CD",
-      secondary: "#F06292",
-      primaryText: "#FFFFFF",
-      secondaryText: "#000000",
-
-      body: "#FFFFFF",
-      paper: "#EDE7F6",
-      text: "#000000",
-      link: "#FFEB3B",
-
-      header: "#D1C4E9",
-      footer: "#D1C4E9",
-      stripe: "#BA68C8",
-      headerText: "#4A148C",
-      footerText: "#4A148C",
-      stripeText: "#FFFFFF",
-    },
-  },
-];
-
 const SettingsForm = () => {
   const { user, setUser, settings, setSettings } = useContext(UserContext);
   console.log("🚀 ~ file: SettingsForm.js:55 ~ SettingsForm ~ user:", user);
@@ -471,10 +227,7 @@ const SettingsForm = () => {
 
     links: [],
   });
-  console.log(
-    "🚀 ~ file: UserSettings.js ~ line 9 ~ UserSettings ~ data",
-    data
-  );
+
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -495,10 +248,7 @@ const SettingsForm = () => {
       }
     }
   }, [settings]);
-  console.log(
-    "🚀 ~ file: SettingsForm.js:55 ~ useEffect ~ settings:",
-    settings
-  );
+  console.log("🚀 ~ file: SettingsForm.js:55 ~ useEffect ~ settings:", settings);
 
   function save() {
     const formData = new FormData();
@@ -509,9 +259,6 @@ const SettingsForm = () => {
     if (data.logoUpdated) formData.append("logo", logoFile);
     if (data.coversUpdated) formData.append("covers", coversFiles);
     if (data.footerUpdated) formData.append("footer", footerFile);
-    // formData.append("logo", data.logo);
-    // formData.append("covers", data.covers);
-    // formData.append("footer", data.footer);
 
     DataService.upload(formData)
       .then((res) => {
@@ -554,16 +301,6 @@ const SettingsForm = () => {
         <Box className="themeCBF" sx={{ height: "72px" }} />
         <Container>
           <h1>Website Theme Settings</h1>
-          {/* <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-        defaultValue="default"
-    >
-        <FormControlLabel value="default" control={<Radio />} label="Default" />
-        <FormControlLabel value="dark" control={<Radio />} label="Dark" />
-        <FormControlLabel value="blue" control={<Radio />} label="Blue" />
-    </RadioGroup> */}
 
           <Box sx={{ width: "100%" }}>
             <Box
@@ -775,16 +512,6 @@ const SettingsForm = () => {
                       justifyContent: "space-around",
                     }}
                   >
-                    {/* {showEdit ? <>
-                            <Button variant="contained" onClick={saveSettings} size="small" color="success" sx={{width: '6rem'}}>
-                                Save
-                            </Button>
-                            <Button variant="contained" onClick={() => setShowEdit(false)} size="small" color="error" sx={{width: '6rem'}}>
-                                Cancel
-                            </Button>
-                        </>: <Button variant="contained" onClick={() => setShowEdit(true)} size="small" color="primary" sx={{width: '6rem'}}>
-                            Modify    
-                        </Button>} */}
                   </Box>
                 </Box>
               </Box>
@@ -798,53 +525,6 @@ const SettingsForm = () => {
                 Upload Image Files
               </Typography>
               <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                {/* <Paper sx={{ p: 2, width: '100%' }} elevation={1}>
-                <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
-                    {covers.map((image, index) => (
-                    <Card sx={{ maxWidth: 100, mb:2 }} key={image}>
-                        <CardMedia
-                            component="img"
-                            height="150"
-                            image={image}
-                            alt="activity image"
-                        />
-                        <CardActions>
-                            <Button 
-                                size="small"
-                                onClick={(e) => {
-                                    const newCovers = [...covers]
-                                    newCovers.splice(index, 1)
-                                    setCovers(newCovers)
-                                }}
-                                color="error"
-                                variant="contained"
-                            >
-                                Remove
-                            </Button>
-                        </CardActions>
-                    </Card>
-                ))}
-                </Box>
-                <label htmlFor="image-upload">
-                    <input
-                        id="image-upload"
-                        name="image-upload"
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => {
-                            const files = e.target.files
-                            setCoversFiles(prev => prev.concat(Array.from(files)))
-                            setCovers(prev => prev.concat(Array.from(files).map(file => URL.createObjectURL(file))))
-                        }}
-                        multiple
-                        hidden
-                    />
-                    <Button variant="contained" component="span" size="small" color="success">
-                        {covers.length > 0 ? "Add More Images" : "Add Images"}
-                    </Button>
-                </label>
-            </Paper> */}
-
                 <Paper elevation={1} sx={{ padding: 2, width: "100%" }}>
                   {logo && (
                     <Card sx={{ maxWidth: 100, mb: 2 }}>
@@ -902,55 +582,10 @@ const SettingsForm = () => {
                     </Button>
                   </label>
                 </Paper>
-
-                {/* <Paper elevation={1} sx={{padding: 2, width: "100%"}}>
-                {footer && <Card sx={{ maxWidth: 100, mb:2 }}>  
-                    <CardMedia
-                        component="img"
-                        // height="150"
-                        image={footer}
-                        alt="Footer Image"
-                    />
-                    <CardActions>
-                        <Button
-                            size="small"
-                            onClick={()=>{
-                                setFooter(null)
-                                setFooterFile(null)
-                                setData({...data, footer: null, footerUpdated: true})
-                            }}
-                            color="error"
-                            variant="contained"
-                        >
-                            Remove
-                        </Button>
-                    </CardActions>
-                </Card>}
-                <label htmlFor="footer-upload">
-                    <input
-                        id="footer-upload"
-                        name="footer-upload"
-                        type="file"
-                        accept="image/*"
-                        onChange={(e)=>{
-                            setFooterFile(e.target.files[0])
-                            setFooter(URL.createObjectURL(e.target.files[0]))
-                            setData({...data, footer: e.target.files[0], footerUpdated: true})
-                        }}
-                        hidden
-
-                    />
-                    <Button variant="contained" component="span" size="small" color="success">
-                        {footer ? "Change Footer" : "Upload Footer"}
-                    </Button>
-                </label>
-
-            </Paper> */}
               </Box>
               <Button variant="contained" onClick={() => save(data)}>
                 Upload and Save
               </Button>
-              {/* <Divider sx={{ my: 2 }} /> */}
             </Box>
           </Box>
         </Container>
